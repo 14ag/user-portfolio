9aa355bb3e3b3110dc81d9a5f72094de3c3b93db
14ag <muriukipn@gmail.com>
2026-02-24 14:51:21 +0300

    24 feb

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Dict, Any
+from typing import Dict
+import json
 
 
-
-
-
+class Content(BaseModel):
+    title: str
+    body: str
 
 
 app = FastAPI()
+
 
 @app.get("/")
 async def test_function():
@@ -17,7 +19,7 @@ async def test_function():
 
 
 @app.post("/message")
-async def receive_message(message_body: Dict[str,str]):
+async def receive_message(message_content: Dict[str,str]):
     try:
         #send message using google keep api
         return
@@ -27,11 +29,52 @@ async def receive_message(message_body: Dict[str,str]):
 
 
 
-
-@app.put("/projects")
-async def receive_projects(message_body: Dict[str,str]):
+# update project list
+@app.post("/projects")
+async def receive_projects(project_content: Dict[str,str]):
     try:
-        #send message using google keep api
+        try:
+            with open(r"data.json", "rw") as project_data_file:
+                project_data_file_content=json.loads(project_data_file.read())
+                project_data_file_content[]=project_content[]
+                project_data_file.write(json.dumps(project_data_file_content))
+
+        except:
+            return
+        return
+    except:
+        return {"message":"failiure"}
+    
+
+# remove a project
+@app.delete("/projects")
+async def remove_project(project_content: Dict[str,str]):
+    try:
+        try:
+            with open(r"data.json", "rw") as project_data_file:
+                project_data_file_content=json.loads(project_data_file.read())
+                project_data_file_content[]
+                project_data_file.write(json.dumps(project_data_file_content))
+
+        except:
+            return
+        return
+    except:
+        return {"message":"failiure"}
+    
+
+# modify a project
+@app.put("/projects")
+async def modify_projects(project_content: Dict[str,str]):
+    try:
+        try:
+            with open(r"data.json", "rw") as project_data_file:
+                project_data_file_content=json.loads(project_data_file.read())
+                project_data_file_content[]=project_content[]
+                project_data_file.write(json.dumps(project_data_file_content))
+
+        except:
+            return
         return
     except:
         return {"message":"failiure"}
